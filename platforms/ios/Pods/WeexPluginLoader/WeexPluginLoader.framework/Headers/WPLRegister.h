6ec5efcbb0442856cb876b8114334497a59ca999
zwwill <zwwill7@163.com>
2018-05-10 17:51:13 +0800

    update ios pf

---
+++ platforms/ios/Pods/WeexPluginLoader/WeexPluginLoader.framework/Headers/WPLRegister.h
@@ -0,0 +1,15 @@
+//
+//  WPRegister.h
+//  WeexPluginDemo
+//
+//  Created by 齐山 on 17/3/7.
+//  Copyright © 2017年 taobao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WPRegister : NSObject
+
++(void )registerPlugins;
+
+@end
